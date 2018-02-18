2572059f1112101162708ada38dafcfcadc6242f
Rafael Wayar <rafael.wt@gmail.com>
2018-02-18 12:19:35 -0400

    Primer Commit Seccion 4 Clase 25

---
+++ src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {HttpModule } from '@angular/http';
+//rutas
+import{ app_routing } from './app.routes'
+//servicios
+
+import { InformacionService } from './services/informacion.service'
+
+//componentes
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { PortafolioComponent } from './components/portafolio/portafolio.component';
+import { AboutComponent } from './components/about/about.component';
+import { ProductoComponent } from './components/producto/producto.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    PortafolioComponent,
+    AboutComponent,
+    ProductoComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    app_routing
+    
+  ],
+  providers: [
+    InformacionService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+import { InformacionService } from '../../services/informacion.service'
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html'
+})
+export class HeaderComponent {
+
+  constructor(public _is:InformacionService){
+
+  }
+
+}
