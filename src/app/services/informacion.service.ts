6853c4d74dd4e5da9efb0a02317d88e15e7c241f
Rafael Wayar <rafael.wt@gmail.com>
2018-02-18 20:29:42 -0400

    Servicio de firebase listo

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -6,13 +6,29 @@ export class InformacionService {
 
   info: any = {};
   cargada : boolean = false;
+  cargada_sobre_nosotros : boolean = false;
+  equipo:any[] =[];
   constructor(public http:Http) {
 
-    this.http.get("assets/data/info.pagina.json").subscribe(data => {
-         console.log(data.json());
+      this.cargarInfo();
+      this.cargarSobreNosotros();
+
+   }
+
+   public cargarInfo (){
+      this.http.get("assets/data/info.pagina.json").subscribe(data => {
+        //console.log(data.json());
           this.cargada = true;
           this.info = data.json();
-    } )
+      });
+   }
+
+   public cargarSobreNosotros(){
+      this.http.get("https://paginaweb-f5a9e.firebaseio.com/equipo.json").subscribe(data => {
+          console.log(data.json());
+          this.cargada_sobre_nosotros = true;
+          this.equipo = data.json();
+      });
 
    }
 
